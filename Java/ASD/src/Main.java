6e105378e62828bac3aaab5e7ee5281335ef438e
Adrian Bączek <adrian@sharedmasters.com>
2019-10-26 23:04:52 +0200

    Main class with 2 tests data for proper tree building

--- Java/ASD/src/Main.java
+++ Java/ASD/src/Main.java
@@ -3,24 +3,18 @@ import map.rbt.RedBlackTree;
 public class Main {
 
     public static void main(String[] args) {
-        RedBlackTree<Integer, Character> rbt = new RedBlackTree();
+        //RedBlackTree<Character, Character> rbt = new RedBlackTree();
+        RedBlackTree<Integer, Integer> rbt = new RedBlackTree();
 
-        rbt.add(1,'a');
+        //char[] values = {'a','l','g','o','r','y','t','m'};
+        //char[] keys = {'a','l','g','o','r','y','t','m'};
+        int[] keys = {156, 202, 243, 734, 58, 47, 73, 121, 49, 256};
+        int[] values = {156, 202, 243, 734, 58, 47, 73, 121, 49, 256};
+
+        for(int i = 0; i < keys.length; i++){
+            rbt.add(keys[i],values[i]);
+        }
+
         rbt.showTree();
-        rbt.add(3,'l');
-        rbt.showTree();
-        rbt.add(2,'g');
-        rbt.showTree();
-/*        rbt.add(5,'o');
-        rbt.showTree();
-        rbt.add(6,'r');
-        rbt.showTree();
-        rbt.add(8,'y');
-        rbt.showTree();
-        rbt.add(7,'t');
-        rbt.showTree();
-        rbt.add(4,'m');
-        rbt.showTree();
-*/
     }
 }
